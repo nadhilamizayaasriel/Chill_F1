e09d29e58a31e0e9f63d0d59e533bcf0cc75add2
nadhilamizayaasriel01 <nadhilamizayaasriel@student.telkomuniversity.ac.id>
2026-09-24 16:37:24 +0700

    userApi

--- services/api/userApi.js
+++ services/api/userApi.js
@@ -4,23 +4,29 @@ const API_URL = import.meta.env.VITE_API_USERS;
 
 export async function getUsers() {
   const response = await axios.get(API_URL);
+
   return response.data;
 }
 
 export async function addUser(userData) {
   const response = await axios.post(API_URL, userData);
+
   return response.data;
 }
 
 export async function updateUser(id, userData) {
   const response = await axios.put(
     `${API_URL}/${id}`,
-    userData,
+    userData
   );
+
   return response.data;
 }
 
 export async function deleteUser(id) {
-  const response = await axios.delete(`${API_URL}/${id}`);
+  const response = await axios.delete(
+    `${API_URL}/${id}`
+  );
+
   return response.data;
 }
